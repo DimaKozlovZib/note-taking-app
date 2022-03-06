6d67e7463f75e335b7e018271c54f5cd06ed854f
Dima <dimaziboc@gmail.com>
2022-03-06 22:37:35 +0300

    переписал с помощью ООП ,добавил строку (у вас нет записей)

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 "use script";
 //localStorage.removeItem("listNotes")
 //.............................................................................................
-
-const menu = document.querySelector("#menu");
-const nav = document.querySelector("#Site-navigation");
-
-//.............................................................................................
-
-menu.onclick = () => {
-    if (!(menu.classList.contains("is-active"))) {
-        menu.classList.add("is-active");
-        nav.classList.add("is-open");
-    } else {
-        menu.classList.remove("is-active");
-        nav.classList.remove("is-open");
+class HTMLelemntsForApp {
+    _nav = document.getElementById("Site-navigation");
+    _menu = document.getElementById("menu");
+    _add = document.getElementById("add");//button
+    get _nav(){
+        return this._nav;
     }
-};
-
-//.............................................................................................
-
-const add = document.querySelector("#add");
-
-//.............................................................................................
-
-let stopSending = add.onclick = (event) => {
-    return event.preventDefault();
+    get _menu(){
+        return this._menu;
+    }
+    get _add(){
+        return this._add;
+    }
 }
-let addNotes = add.onclick = () => {
-    let noteString = document.querySelector("#input-for-add").value;//form.value
+
+//.............................................................................................
+
+menu.addEventListener("click" , () => {
+    let classVariables = new HTMLelemntsForApp();
+
+    const TO_CLOSE_ITEM = document.querySelectorAll(".to-close");
+    if (!(classVariables._menu.classList.contains("is-active"))) {
+        classVariables._menu.classList.add("is-active");
+        classVariables._nav.classList.add("is-open");
+        TO_CLOSE_ITEM.forEach(i => {i.style.display = "none" });
+    } else {
+        classVariables._menu.classList.remove("is-active");
+        classVariables._nav.classList.remove("is-open");
+        TO_CLOSE_ITEM.forEach(i => {i.style.display = "block" });
+    }
+})
+//.............................................................................................
+
+let stopSending = new HTMLelemntsForApp()._add.onclick = (event) => { return event.preventDefault(); };
+
+let addNotes = new HTMLelemntsForApp()._add.onclick = () => {
+    let noteString = document.getElementById("input-for-add").value;//form.value
 
     if(!noteString) return;
     noteString = localStorage.listNotes ? [localStorage.listNotes , noteString].join("@#") : noteString;//для разделения сторок
@@ -36,90 +46,106 @@ let addNotes = add.onclick = () => {
 
 //.............................................................................................
 
-const noteBox = document.querySelector("#note-box");
+class createHTML{
+    constructor(index , item){
+        this.index = index;
+        this.item = item;
+        this.noteBox = document.querySelector("#note-box");
+    }
+    get createNoteWrapper(){//создаём обёртку для текста и кнопки удаления
+        let box = document.createElement("div");//создаём элемент
+        box.classList += "note";//задаём класс
+        box.setAttribute("data-index" , this.index);
+        this.noteBox.prepend(box);//вставляем в родителя
+        return box;
+    }
+    get createNoteDeleteImg(){//создаём кнопку удаления (картинка)
+        let img = document.createElement("img");
+        img.setAttribute("src" , "image/delete.svg");
+        img.classList += "deleteImg";
+        return img;
+    }
+    get createNoteText(){//создаём элемент с текстом
+        let note = document.createElement("p");//создаём элемент
+        note.classList += "note-text";//задаём класс
+        note.innerHTML = this.item;//задаём значение
+        return note;
+    }
+}
+class hideElement{
+    constructor(noVisible , visible){
+        this.noVisible = noVisible;
+        this.visible = visible;
+    }
+    get hideElements(){
+        document.querySelectorAll(this.noVisible).forEach(i => {i.classList.remove("is-working")});
+        document.querySelectorAll(this.visible).forEach(i => {i.classList.add("is-working")});
+    }
+}
+class deleteNote{
+    #deleteImg = document.querySelectorAll('.deleteImg');//иконка удаления
+    constructor(array){
+        this.array = array;
+    }
+    get main(){
+        this.#deleteImg.forEach(item => {
+            item.addEventListener("click", () => {//слушатель кликов на иконку удаления
+                makeElementsInvisible();//делаем видимым окно для подтверждения удаления
 
-//.............................................................................................
-window.onload = function build_HTML_elements(){
-    if (!localStorage.listNotes) return;
+                document.getElementById("confirm-button").addEventListener("click",() => {//слушаем кнопку подтверждения
+                    item.parentElement.style.display = "none";// делаем невидимым удаляемый элемент
+                    // item.parentElement - обёртка кнопки удаления с индексом
+                    this.array.splice( item.parentElement.getAttribute("data-index") , 1)
+                    localStorage.setItem("listNotes" ,  this.array.join("@#"));
 
+                    makeElementsInvisible();
+                    this.#СhangeIndexAllPreviousElements(item);
+
+                    if (!localStorage.listNotes){ hideElements("#load-icon" ,".noNotes-wrapper");}//включение строки 'нету записей' 
+                })
+
+                
+                document.getElementById("cancel").onclick = function(){
+                    makeElementsInvisible();
+                }
+            });
+        });
+    }
+    #СhangeIndexAllPreviousElements(item){
+        let sibling = item.parentElement;
+    
+        while(sibling.previousElementSibling){
+            //получаем соседний элемент и меняем атрибут (index - 1)
+            sibling = sibling.previousElementSibling
+            sibling.setAttribute("data-index" , sibling.getAttribute("data-index")-1);
+        }
+    }
+}
+
+let build_HTML_elements = new Promise(function (){
+    if (!localStorage.listNotes){
+        new hideElement("#load-icon" ,".noNotes-wrapper").hideElements;
+        return;
+    };
 
     let stringList = localStorage.listNotes.split("@#");//notes array
      
     for (let [index , item] of stringList.entries()) {
-        let box = createNoteWrapper(index);//создаём элемент
-        let note = createNoteText(item);//создаём элемент
-        let img = createNoteDeleteImg();
+        let HTMLelem = new createHTML(index , item);//создаём класс
+
+        let box = HTMLelem.createNoteWrapper;//создаём элемент
+        let note = HTMLelem.createNoteText;//создаём элемент
+        let img = HTMLelem.createNoteDeleteImg;
 
         box.append(img);
         box.prepend(note);//вставляем в родителя
     }
-    hideLoadIcon();
-    deleteNote(stringList);
-}
-function createNoteWrapper(index) {
-    let box = document.createElement("div");//создаём элемент
-    box.classList += "note";//задаём класс
-    box.setAttribute("data-index" , index);
-    noteBox.prepend(box);//вставляем в родителя
-    return box;
-}
-function createNoteDeleteImg() {
-    let img = document.createElement("img");
-    img.setAttribute("src" , "image/delete.svg");
-    img.classList += "deleteImg";
-    return img;
-}
-function createNoteText(item){
-    if(!item) return;// !== undefined , !== ""
-
-    let note = document.createElement("p");//создаём элемент
-    note.classList += "note-text";//задаём класс
-    note.innerHTML = item;//задаём значение
-    return note;
-}
-function hideLoadIcon() {
-    document.querySelector("#load-icon").classList.remove("is-working");
-    document.querySelectorAll(".note").forEach(i => {i.classList.add("is-working")});
-}
+    new hideElement("#load-icon" ,".note").hideElements;
+    new deleteNote(stringList).main;
+})
 
 
-function deleteNote(array) {
-    const deleteImg = document.querySelectorAll('.deleteImg');//иконка удаления
 
-    deleteImg.forEach(item => {
-        item.addEventListener("click", () => {//слушатель кликов на иконку удаления
-            makeElementsInvisible();//делаем видимым окно для подтверждения удаления
-
-            document.querySelector("#confirm-button").onclick = function(){//слушаем кнопку подтверждения
-                item.parentElement.style.display = "none";// делаем невидимым удаляемый элемент
-                // item.parentElement - обёртка item'а с индексом
-                array.splice( item.parentElement.getAttribute("data-index") , 1)
-                localStorage.setItem("listNotes" , array.join("@#"));
-
-                makeElementsInvisible();
-                getAllPreviousElements(item);
-            }
-            document.querySelector("#cancel").onclick = function(){
-                makeElementsInvisible();
-            }
-        });
-    });
-}
-function getAllPreviousElements(item){
-    let allPreviousElements = []
-    let sibling = item.parentElement;
-
-    while(sibling.previousElementSibling){
-        sibling = sibling.previousElementSibling;
-        allPreviousElements.push(sibling);
-    }
-    indexСhange(allPreviousElements);
-}
-function indexСhange(array){
-    array.forEach(item => {
-        item.setAttribute("data-index" , item.getAttribute("data-index")--);
-    })
-}
 function makeElementsInvisible() {
     const windowConfirm = document.querySelector(".window-confirm-deletion");
 
